9098709b15601f2c009806484099793bec434864
Unknown <samodelkinv@gmail.com>
2018-12-14 14:57:33 +0500

    + Field

--- src/Field.ts
+++ src/Field.ts
@@ -1,3 +1,13 @@
+import {GameObject} from "./entities/base/GameObject";
+
 export class Field {
-    
+    objects: Array<Array<GameObject>>;
+    width: number;
+    height: number;
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.objects = Array.from({length: this.width}).map(_ => Array.from({length: this.height}))
+    }
 }
