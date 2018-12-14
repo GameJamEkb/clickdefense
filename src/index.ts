3719c156c39028077073d9b9a9ef54b4b8a79ad5
Alexey Morozov <alexeymorozovv@yandex.ru>
2018-12-14 13:45:36 +0500

    tsconfig

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,3 @@ canvas.height = 480;
 const ctx = canvas.getContext('2d');
 
 document.body.appendChild(canvas);
-Object.assign(document.body.style, {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-});
