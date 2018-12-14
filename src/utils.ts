b002de2472714c90eb873c4f6cb9fc2bfc5eb599
samovit <samodelkinv@gmail.com>
2018-12-14 16:00:30 +0500

    + cells from vector

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
+import {Vector} from "./entities/base/Vector";
+import {Cell} from "./entities/base/Cell";
+import {Field} from "./Field";
+
 export function pifagor(x: number, y: number) {
     return Math.sqrt(x * x + y * y);
+}
+
+
+function cell(v: Vector, field: Field): Cell {
+    return new Vector(
+        Math.floor(v.x / field.cellSize),
+        Math.floor(v.y / field.cellSize)
+    )
 }
