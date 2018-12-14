94830601bd1bab4288d27b02bd960b0901346828
samovit <samodelkinv@gmail.com>
2018-12-14 23:07:32 +0500

    + offset

--- src/constants/GameConfig.ts
+++ src/constants/GameConfig.ts
@@ -4,4 +4,12 @@ export class GameConfig {
     static CellSize: number = 32;
 
     static StartGold: number = 100;
+
+    static GameFieldTranslateX: number = 20;
+    static GameFieldTranslateY: number = 50;
+
+
+    static CanvasWidth: number = 800;
+    static CanvasHeight: number = 600;
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import './style.css';
 import {Game} from "./Game";
 import {GameObjectFactory} from "./entities/factories/GameObjectFactory";
+import {GameConfig} from "./constants/GameConfig";
 
 const canvas = document.createElement('canvas');
 
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = GameConfig.CanvasWidth;
+canvas.height = GameConfig.CanvasHeight;
 
 const ctx = canvas.getContext('2d');
 if (ctx == null) {
@@ -20,10 +21,11 @@ sprite.onload = () => {
     GameObjectFactory.sprite = sprite;
 
     const game = new Game(0);
+    ctx.translate(GameConfig.GameFieldTranslateX, GameConfig.GameFieldTranslateY);
     let prevTime = Date.now();
     const loop = () => {
         // @ts-ignore
-        ctx.clearRect(0, 0, 640, 480);
+        ctx.clearRect(-GameConfig.GameFieldTranslateX, -GameConfig.GameFieldTranslateY, canvas.width - GameConfig.GameFieldTranslateX, canvas.height - GameConfig.GameFieldTranslateY);
         const currentTime = Date.now();
         const elapsed = (currentTime - prevTime) / 1000;
         prevTime = currentTime;
