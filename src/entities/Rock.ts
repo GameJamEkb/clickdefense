44c27668bf94f22f61ec539e476cfa0c1d0e3dfc
samovit <samodelkinv@gmail.com>
2018-12-14 15:44:56 +0500

    + Rock

--- src/entities/Rock.ts
+++ src/entities/Rock.ts
@@ -1,8 +1,10 @@
 import {GameObject} from "./base/GameObject";
+import {Vector} from "./base/Vector";
 
 export class Rock extends GameObject {
-    constructor() {
-        super()
+    private static BaseHp = 100;
 
+    constructor(x: number, y: number) {
+        super(new Vector(x, y), Rock.BaseHp, false);
     }
 }
