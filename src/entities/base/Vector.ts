c66afad948ab07d8a75ddb7b858dea34067d647c
Unknown <samodelkinv@gmail.com>
2018-12-14 14:52:03 +0500

    + Vector

--- src/entities/base/Vector.ts
+++ src/entities/base/Vector.ts
@@ -1,3 +1,31 @@
+import {pifagor} from "../../utils";
+
 export class Vector {
-    
+    x : number;
+    y : number;
+
+    constructor(x? : number, y? : number) {
+        this.x = x ? x : 0;
+        this.y = y ? y : 0;
+    }
+
+    add(v: Vector): Vector {
+        return new Vector(this.x + v.x, this.y + v.y);
+    }
+
+    dec(v: Vector) : Vector {
+        return new Vector(this.x - v.x, this.y - v.y);
+    }
+
+    mult(k: number) : Vector {
+        return new Vector(this.x * k, this.y * k);
+    }
+
+    div(k: number) : Vector {
+        return new Vector(this.x / k, this.y / k);
+    }
+
+    length() : number {
+        return pifagor(this.x, this.y);
+    }
 }
